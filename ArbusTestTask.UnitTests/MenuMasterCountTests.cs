7be111ed0ffde75e0595fc4055856eb03373c88e
Artem Levin <leoartemlev@yandex.ru>
2022-11-28 22:40:36 +0500

    split class with tests into several

---
+++ ArbusTestTask.UnitTests/MenuMasterCountTests.cs
@@ -0,0 +1,28 @@
+﻿using ArbusTestsTask;
+using FluentAssertions;
+
+namespace ArbusTestTask.UnitTests;
+
+public partial class MenuMasterTests
+{
+    [Fact]
+    public void Count_ReturnsTotalDishesCount()
+    {
+        // arrange
+        var dishes = new List<string>()
+        {
+            "Dish_1",
+            "Dish_2",
+            "Dish_3"
+        };
+        var expectedResult = dishes.Count;
+
+        var sut = new MenuMaster(dishes, 2);
+
+        // act
+        var result = sut.Count;
+
+        // assert
+        result.Should().Be(expectedResult);
+    }
+}
