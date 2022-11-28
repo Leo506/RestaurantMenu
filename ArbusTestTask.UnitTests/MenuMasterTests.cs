0c19d79c009eabe3e4c4026d2ff32054104b1d75
Artem Levin <leoartemlev@yandex.ru>
2022-11-28 22:00:14 +0500

    created MenuMaster class

---
+++ ArbusTestTask.UnitTests/MenuMasterTests.cs
@@ -0,0 +1,66 @@
+using ArbusTestsTask;
+using FluentAssertions;
+
+namespace ArbusTestTask.UnitTests;
+
+public class MenuMasterTests
+{
+    [Fact]
+    public void MenuMaster_ProvideDishesCollectionAndItemsCountPerPage_Success()
+    {
+        // arrange
+        var dishes = new List<string>()
+        {
+            "Dish_1",
+            "Dish_2",
+            "Dish_3"
+        };
+
+        var itemsPerPage = 2;
+        
+        // act
+        var sut = new MenuMaster(dishes, itemsPerPage);
+        
+        // assert
+        sut.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void MenuMaster_DishesCollectionIsNull_ThrowException()
+    {
+        // arrange
+        const int itemsPerPage = 2;
+
+        // act and assert
+        Assert.Throws<ArgumentNullException>(() => new MenuMaster(null!, itemsPerPage));
+
+    }
+
+    [Fact]
+    public void MenuMaster_ProvideEmptyDishesCollection_ThrowException()
+    {
+        // arrange
+        var dishes = new List<string>();
+        const int itemsPerPage = 2;
+
+        // act and assert
+        Assert.Throws<ArgumentException>(() => new MenuMaster(dishes, itemsPerPage));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void MenuMaster_ItemsPerPageLessThanOne_ThrowException(int itemsPerPage)
+    {
+        // arrange
+        var dishes = new List<string>()
+        {
+            "Dish_1",
+            "Dish_2",
+            "Dish_3"
+        };
+
+        // act and assert
+        Assert.Throws<ArgumentException>(() => new MenuMaster(dishes, itemsPerPage));
+    }
+}
---
+++ ArbusTestsTask/MenuMaster.cs
@@ -0,0 +1,13 @@
+﻿namespace ArbusTestsTask;
+
+public class MenuMaster
+{
+    public MenuMaster(IEnumerable<string> dishes, int itemsPerPage)
+    {
+        if (!dishes.Any())
+            throw new ArgumentException($"The {nameof(dishes)} parameter can not be empty");
+
+        if (itemsPerPage < 1)
+            throw new ArgumentException($"The {nameof(itemsPerPage)} parameter can not be less than 1");
+    }
+}
