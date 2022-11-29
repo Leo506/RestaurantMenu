d0634a3097f041a2d1b69fa1b3826e12c86219f8
Artem Levin <leoartemlev@yandex.ru>
2022-11-29 22:10:12 +0500

    write a demonstrate console program

--- ArbusTestsTask/Program.cs
+++ ArbusTestsTask/Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ArbusTestsTask;
+
+Console.WriteLine("Creating MenuMaster object with following food collection:");
+var food = new List<string>()
+{
+    "Matcha", 
+    "Latte", 
+    "Smoothie", 
+    "Gin", 
+    "Popsicle"
+};
+Console.Write('\t');
+foreach (var item in food)
+{
+    Console.Write(item + ' ');
+}
+Console.WriteLine();
+
+var menuMaster = new MenuMaster<string>(food, 2);
+Console.WriteLine($"MenuMaster count of dishes: {menuMaster.Count}");
+
+Console.WriteLine($"MenuMaster pages count: {menuMaster.PagesCount}");
+
+Console.WriteLine("Count of food on every page:");
+for (var i = 0; i < menuMaster.PagesCount; i++)
+{
+    Console.WriteLine($"Page {i} - food count: {menuMaster.GetDishesCountOnPage(i)}");
+}
+
+Console.WriteLine("Dishes on every page:");
+for (var i = 0; i < menuMaster.PagesCount; i++)
+{
+    Console.Write($"\tPage {i}: ");
+    foreach (var dish in menuMaster.GetDishesOnPage(i))
+    {
+        Console.Write(dish + ' ');
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("First dishes on every page:");
+foreach (var dish in menuMaster.GetFirstDishesOnPages())
+{
+    Console.Write(dish + ' ');
+}
